381291db623091c7cc050e818cf663c855ebd852
DeclanMcGauley <73233273+DeclanMcGauley1@users.noreply.github.com>
2020-12-30 02:54:06 +0000

    written test cases

---
+++ SimpleDotComTestDriver.java
@@ -0,0 +1,18 @@
+public class SimpleDotComTestDriver {
+
+  public static void main(String[] args) {
+    SimpleDotCom simple = new SimpleDotCom();
+
+    int[] location = {2,3,4};
+    simple.setLocation(location);
+
+    String result = simple.checkYourself("2");
+    String testResult = "False";
+
+    if result.equals("hit") {
+      testResult = "True";
+    }
+
+    System.out.println(testResult);
+  }
+}
